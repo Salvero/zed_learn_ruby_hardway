5422445d060d79554a46ee3e1799f6b6e45aa137
Salman Akram <thunder9ruby@gmail.com>
2015-10-06 17:19:09 -0400

    add more exercises

---
+++ exercise_41.rb
@@ -0,0 +1,30 @@
+#Exercise 41
+
+"Make a class named ??? that is-a Y."
+ > class W(R)
+   end
+
+"class ??? has-a initialize that takes ??? parameters."
+> class W
+   def initialize(R)
+   end
+  end
+
+"class ??? has-a function named ??? that takes ??? parameters."
+> class W
+   def R(N)
+   end
+  end
+"Set foo to an instance of class ???."
+> foo = W()
+
+"From foo get the ??? function, and call it with parameters ???."
+> foo.R(N)
+
+"From foo get the ??? attribute and set it to ???."
+> foo.R = N
+
+
+#Take oop_test.rb
+- Reading more codes to learn and translate object-oriented phrases into English translations.
+- Run terminal and typing - "ruby oop_test.rb"
---
+++ exercise_43.rb
@@ -0,0 +1,32 @@
+# Find nouns/verbs
+
+Alien
+Player
+Ship
+Maze
+Room
+-Scene
+Gothon
+Planet
+-Map
+-Engine
+   Death
+   Central Corridor
+   Laser Weapon Armory
+   The Bridge
+   Escape Pod
+
+#Choose these words
+
+Map
+ - next_scene
+ - opening_scene
+Engine
+ - play
+Scene
+ - enter
+ - Death
+ - Central Corridor
+ - Laser Weapon Armory
+ - The Bridge
+ - Escape Pod
